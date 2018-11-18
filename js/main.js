15f597a70ac02c94bc13f8a664a9154429f2357e
Amrendra <amrendrapcs07@gmail.com>
2018-11-18 18:00:57 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('./sw_cached_site.js')
+      .register('../sw_cached_site.js')
       .then(reg => console.log('service Worker registered'))
       .catch(err => console.log(`service worker err, ${err}`));
   });
